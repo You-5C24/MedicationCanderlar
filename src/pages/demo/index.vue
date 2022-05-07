ce22a86ca81ea833de014e7f98a95dad871527a3
5C24 <you_5c24@163.com>
2022-05-07 15:59:38 +0800

    feat: uno rules config

--- src/pages/demo/index.vue
+++ src/pages/demo/index.vue
@@ -1,5 +1,17 @@
 <template>
-  <span>Page Demo</span>
+  <button
+    p="y-2 x-4"
+    font="semibold"
+    shadow="lg"
+    text="white"
+    bg="green-500 hover:green-700"
+    border="rounded-lg none "
+    cursor="pointer"
+  >
+    Click me
+  </button>
+  <div class="i-ic-baseline-add-circle-outline text-50 bg-green-500" />
+  <div class="my-8">test uno rules</div>
 </template>
 
 <script lang="ts">
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,32 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
+import Unocss from 'unocss/vite';
+import { presetUno, presetAttributify, presetIcons } from 'unocss';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Unocss({
+      presets: [presetUno(), presetAttributify(), presetIcons()],
+      rules: [
+        [/^text-(\d+)$/, ([, d]) => ({ 'font-size': `${d}px` })],
+        [/^m-(\d+)$/, ([, d]) => ({ margin: `${d}px` })],
+        [/^mx-(\d+)$/, ([, d]) => ({ margin: `0 ${d}px 0 ${d}px` })],
+        [/^my-(\d+)$/, ([, d]) => ({ margin: `${d}px 0 ${d}px 0` })],
+        [/^mt-(\d+)$/, ([, d]) => ({ 'margin-top': `${d}px` })],
+        [/^mb-(\d+)$/, ([, d]) => ({ 'margin-bottom': `${d}px` })],
+        [/^ml-(\d+)$/, ([, d]) => ({ 'margin-left': `${d}px` })],
+        [/^mr-(\d+)$/, ([, d]) => ({ 'margin-right': `${d}px` })],
+        [/^p-(\d+)$/, ([, d]) => ({ padding: `${d}px` })],
+        [/^pt-(\d+)$/, ([, d]) => ({ 'padding-top': `${d}px` })],
+        [/^pb-(\d+)$/, ([, d]) => ({ 'padding-bottom': `${d}px` })],
+        [/^pl-(\d+)$/, ([, d]) => ({ 'padding-left': `${d}px` })],
+        [/^pr-(\d+)$/, ([, d]) => ({ 'padding-right': `${d}px` })]
+      ]
+    })
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
